fac48f9162ed299934fc8e7f0fc53cd1554924d3
Arman Jabari <112120726+ArmanJabari@users.noreply.github.com>
2026-06-09 03:08:48 +0400

    Add code to demo.asm

--- demo.asm
+++ demo.asm
@@ -1,1 +1,23 @@
+org 100h
 
+jmp start
+
+%include "core/VGA.inc"
+%include "draw/pixel.inc"
+
+start:
+    InitVGA
+
+    DrawPixel  160, 100, 15
+    DrawPixel  10,  10,  4
+    DrawPixel  310, 10,  1
+    DrawPixel  10,  190, 2
+    DrawPixel  310, 190, 14
+
+    mov ah, 00h
+    int 16h
+
+    ExitVGA
+
+    mov ax, 4C00h
+    int 21h
