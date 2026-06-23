246ddef8977f6ad996d9fd34a9c1a3ac22bc0021
Arman Jabari <112120726+ArmanJabari@users.noreply.github.com>
2026-06-23 16:48:02 +0400

    Add InitMouse, ShowMouse and HideMouse to demo.asm

--- demo.asm
+++ demo.asm
@@ -8,11 +8,15 @@ jmp start
 %include "draw/line.inc"
 %include "draw/rect.inc"
 %include "input/keyboard.inc"
+%include "input/mouse.inc"
 %include "screen/clear.inc"
 
 start:
     InitVGA
+    InitMouse
+    ShowMouse
 
+    HideMouse
     DrawPixel  160, 100, 15
     DrawPixel  10,  10,  4
     DrawPixel  310, 10,  1
@@ -28,16 +32,22 @@ start:
     DrawRect 10,  10,  15, 100, 50, 0
     DrawRect 50,  50,  12, 80,  40, 1
     DrawRect 100, 100, 14, 60,  30, 0
+    ShowMouse
 
     Delay 1000
 
+    HideMouse
     ClearScreen 0
+    ShowMouse
 
+    HideMouse
     DrawALine 0, 0, 319, 199, 12
     DrawALine 0, 199, 319, 0, 15
+    ShowMouse
 
     WaitKey
 
+    HideMouse
     ExitVGA
 
     mov ax, 4C00h
