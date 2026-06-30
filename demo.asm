4646c568f2dcdb386fa3c9de913218407d1b1947
Arman Jabari <112120726+ArmanJabari@users.noreply.github.com>
2026-07-01 01:13:59 +0400

    Add WaitVSync and GetKey to demo.asm

--- demo.asm
+++ demo.asm
@@ -41,19 +41,51 @@ start:
     ClearScreen 0
     ShowMouse
 
+    mov word [y_pos], 96    
+    mov word [y_dir], 1     
+
+main_loop:
+    WaitVSync
+
     HideMouse
+    DrawRect 156, [y_pos], 0, 8, 8, 1
+
     DrawALine 0, 0, 319, 199, 12
     DrawALine 0, 199, 319, 0, 15
-    RenderSprite 156, 96, 8, 8, smily_face
+
+    mov ax, [y_pos]
+    add ax, [y_dir]
+    mov [y_pos], ax
+
+    cmp ax, 0
+    jle change_dir_down
+    cmp ax, 192
+    jge change_dir_up
+    jmp draw_sprite
+
+change_dir_down:
+    mov word [y_dir], 1
+    jmp draw_sprite
+
+change_dir_up:
+    mov word [y_dir], -1
+
+draw_sprite:
+    RenderSprite 156, [y_pos], 8, 8, smily_face
     ShowMouse
 
-    WaitKey
+    GetKey
+    cmp ax, 0
+    jz main_loop
 
     HideMouse
     ExitVGA
 
     mov ax, 4C00h
     int 21h
+
+y_pos: dw 0
+y_dir: dw 1
 
 smily_face:
     db  0,  0, 14, 14, 14, 14,  0,  0
